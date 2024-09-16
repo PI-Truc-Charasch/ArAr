764add392a0d1ef269374cfe6d82d8e29284c794
PI Truc-Charasch <pitcpro@outlook.com>
2024-09-16 16:06:31 +0200

    Ajout d'un fichier data.R

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' #tholeithe
+#'
+#'
+#'
+#' @format Un data frame avec 23170 lignes and 4 variables:
+#' \describe{
+#'   \item{entity}{Libellé du pays ou de la zone}
+#'   \item{code}{Code du pays ou de la zone}
+#'   \item{year}{Année de l'observation}
+#'   \item{annual_co2_emissions}{Emmissions annuelles de CO2}
+#' }
+#'
+#'
