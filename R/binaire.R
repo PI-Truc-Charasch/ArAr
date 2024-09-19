303615fcfffd107ceeada081cde32c0bb1465a77
PI Truc-Charasch <pitcpro@outlook.com>
2024-09-19 20:40:36 +0200

    Ajout légende à droite

--- R/binaire.R
+++ R/binaire.R
@@ -7,7 +7,7 @@
 #' @return Un biplot du diagramme binaire.
 #' @importFrom utils data
 #' @importFrom khroma color
-#' @importFrom graphics legend points
+#' @importFrom graphics legend points par
 #' @export
 #' @examples
 #' ## dataset de Waksman, 1999
@@ -20,18 +20,18 @@ binaire<-function(data,x,y,color=FALSE){
 
   x_range<-range(data[, i_x]+1, na.rm = TRUE)
   y_range<-range(data[, i_y], na.rm = TRUE)
-
+  par(mar = c(5, 4, 4, 8), xpd = TRUE)
   plot(data[, i_x], data[, i_y], xlab = x, ylab = y, xlim = x_range, ylim = y_range)
 
   #----mise en couleur des groupes----------------------------------------------------
   if (color){
     data_split=split(data[,1:ncol(data)-1],data[,ncol(data)])
     groupes <- unique(data[, ncol(data)])
-    palette<-color("soil")(ncol(data))
+    palette<-color("bright")(7)
     for (i in 1:length(data_split)){
       points(data_split[[i]][, i_x], data_split[[i]][, i_y], col = palette[i], pch = 16)
     }
-    legend("bottomright", legend = groupes, col = palette, pch = 16, title = "Groupes")
+    legend("bottomright", inset = c(-0.2, 0), legend = groupes, col = palette, pch = 16, title = "Groupes",box.lty=0)
   }
 
 }
